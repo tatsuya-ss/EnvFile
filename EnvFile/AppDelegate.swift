83f0f95069167572b2c9eb13a66066c98b9666f7
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-13 22:11:12 +0900

    環境変数読み込み処理

--- EnvFile/AppDelegate.swift
+++ EnvFile/AppDelegate.swift
@@ -13,10 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        loadEnvFile()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -29,6 +29,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
+extension AppDelegate {
+    
+    private func loadEnvFile() {
+        guard let path = Bundle.main.path(forResource: ".env", ofType: nil) else {
+            fatalError("Not found: '/path/to/.env'.\nPlease create .env file reference from .env.sample")
+        }
+        let url = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: url)
+            let dataString = String(data: data, encoding: .utf8) ?? "Empty File"
+//        dataString: TWITTER_CONSUMER_KEY='a'
+//                    TWITTER_CONSUMER_SECRET='b'
+            let clean = dataString.replacingOccurrences(of: "\"", with: "").replacingOccurrences(of: "'", with: "")
+//        clean: TWITTER_CONSUMER_KEY=a
+//               TWITTER_CONSUMER_SECRET=b
+            let envVars = clean.components(separatedBy:"\n")
+//        envVars: ["TWITTER_CONSUMER_KEY=a", "TWITTER_CONSUMER_SECRET=b", "", ""]
+            for envVar in envVars {
+                let keyVal = envVar.components(separatedBy:"=")
+//                keyVal: ["TWITTER_CONSUMER_KEY", "a"]
+//                        ["TWITTER_CONSUMER_SECRET", "b"]
+//                        [""]
+//                        [""]
+                if keyVal.count == 2 {
+                    setenv(keyVal[0], keyVal[1], 1)
+                }
+            }
+        } catch {
+            fatalError(error.localizedDescription)
+        }
     }
 
 
--- EnvFile/ViewController.swift
+++ EnvFile/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let env = ProcessInfo.processInfo.environment
+        print(#function, env["TWITTER_CONSUMER_KEY"], env["TWITTER_CONSUMER_SECRET"])
     }
 
 
